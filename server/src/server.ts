808aad050eeb77b0706f62cfd6b6b6879fdfddd8
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-07-07 23:21:24 -0300

    install drizzle and created connections, migrations and schemas

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,6 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { sql } from './db/connection.ts'
 import { env } from './env.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
