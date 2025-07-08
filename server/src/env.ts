c162300815a14321ce900b383bf759ebe96d2c22
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-07-07 22:41:12 -0300

    feat: create docker compose with pgvector, install postgres and create a postgres connection

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,5 +1,8 @@
-import { z } from "zod";
+import { z } from 'zod'
 
-const envSchema = z.object({ PORT: z.coerce.number().default(3333) });
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().url().startsWith('postgresql://'),
+})
 
-export const env = envSchema.parse(process.env);
+export const env = envSchema.parse(process.env)
